010d836f379caa64a0bd37b07e55861e8002d90c
bonfield2l <lucy.bonfield2@paddypowerbetfair.com>
2023-12-01 18:29:48 +0000

    added to js to add the number of cards dependant on how many days are in the month, and removed the hard coded ones from the html

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,11 +5,31 @@ document.addEventListener("DOMContentLoaded", function () {
         dayElement.addEventListener('click', function () {
             var dayOfMonth = dayElement.querySelector('.card-title').innerText;
             localStorage.setItem('selectedDay', dayOfMonth); // selected day is stored in dayOfMonth var
-            console.log(dayOfMonth);
         });
     });
 });
 
-// Display month and year
-var date = dayjs().format("MMMM YYYY");
-$("#today").text(date);
+document.addEventListener("DOMContentLoaded", function () {
+    // Displays todays date
+    var date = dayjs().format("MMMM YYYY");
+    $("#today").text(date);
+    var totalDays = dayjs().daysInMonth();
+    var calendarContainer = $("#calendar");   
+    // Adds cards based on the number of days in current month
+    for (var day = 1; day <= totalDays; day++) {
+        var newCard = $('<div class="card day" style="width: 9rem; height: 9rem;">' +
+                            '<div class="card-body">' +
+                                '<h5 class="card-title">' + day + '</h5>' +
+                                '<p class="card-text"></p>' +
+                                '<img src="assets/images/add.png" class="card-img add-icon mx-auto" style="display: none;" alt="...">' +
+                                '<a href="#" class="stretched-link"></a>' +
+                            '</div>' +
+                        '</div>');
+        // Date is stored locally when card is clicked
+        newCard.click(function () {
+            var selectedDay = $(this).find('.card-title').text();
+            localStorage.setItem('selectedDay', selectedDay);
+        });
+        calendarContainer.append(newCard);
+    }
+});
