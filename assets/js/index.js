b417863768157db5d5b833e1cefe99cbc6da77fe
bonfield2l <lucy.bonfield2@paddypowerbetfair.com>
2023-12-01 13:47:42 +0000

    added comment

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     dayElements.forEach(function (dayElement) {
         dayElement.addEventListener('click', function () {
             var dayOfMonth = dayElement.querySelector('.card-title').innerText;
-            localStorage.setItem('selectedDay', dayOfMonth);
+            localStorage.setItem('selectedDay', dayOfMonth); // selected day is stored in dayOfMonth var
             //console.log(dayOfMonth);
         });
     });
