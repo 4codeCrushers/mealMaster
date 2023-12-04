049194276d58ecc6af9ef58ada460ff4e4f70140
Ewelina Sobora <e@e-dot.uk>
2023-12-04 16:46:06 +0000

    add calculations for calories

--- assets/js/mealList.js
+++ assets/js/mealList.js
@@ -1,15 +1,21 @@
+const apiKey = "600998a5ce3a47579fa28a23d0ddb36d";
 const searchInput = $("input");
 const searchBtn = $("#search-btn");
+const mealList = $("#meal-list");
 
-localStorage.setItem("goal", JSON.stringify({ calories: 2000, carbs: 200, fat: 50, protein: 100 }));
+const goal = {
+  goal: { calories: 2000, carbs: 200, fat: 50, protein: 100 },
+  leftover: { calories: 2000, carbs: 10, fat: 30, protein: 60 }
+};
+// parse queries from local storage or create empty array
+const queries = JSON.parse(localStorage.getItem("queries")) || {};
+localStorage.setItem("queries", JSON.stringify(queries));
 
 // Event listener for search button
 $("form").on("submit", function (event) {
   event.preventDefault();
-  const queries = JSON.parse(localStorage.getItem("queries")) || [];
-  localStorage.setItem("queries", JSON.stringify(queries));
+
   const query = searchInput.val();
-  console.log(query);
   const url = `https://api.spoonacular.com/recipes/guessNutrition?title=${query}&apiKey=${apiKey}`;
 
   fetch(url, {
@@ -19,15 +25,74 @@ $("form").on("submit", function (event) {
   })
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-      const queryHash = {};
-      queryHash[query] = { calories: data.calories.value, carbs: data.carbs.value, fat: data.fat.value, protein: data.protein.value };
 
-      let queries = JSON.parse(localStorage.getItem("queries")) || [];
-      queries.push(queryHash);
-      localStorage.setItem("queries", JSON.stringify(queries));
-    })
-    .catch(error => {
-      console.error(error);
-    });
+      saveQuery(query, data);
+
+      showCards();
+    }
+    );
 });
+
+function saveQuery(query, data) {
+  let queries = JSON.parse(localStorage.getItem("queries")) || {};
+
+  if (queries.hasOwnProperty(query)) {
+    alert(`${query} has already been added to the meal`);
+    return false;
+  }
+
+  if (!hasLeftoverCalories(queries, data.calories.value)) {
+    alert(`You don't have enough calories for ${query}`);
+    return false;
+  }
+
+  queries[query] = {
+    calories: data.calories.value,
+    carbs: data.carbs.value,
+    fat: data.fat.value,
+    protein: data.protein.value
+  };
+
+  localStorage.setItem("queries", JSON.stringify(queries));
+}
+
+function hasLeftoverCalories(existingQueries, newQueryCalories) {
+  let leftover = goal.goal.calories;
+
+  // Loop through all existing query items and subtract calories from leftover
+  for (var [key, value] of Object.entries(existingQueries)) {
+    leftover -= value.calories;
+  }
+
+  if (leftover >= newQueryCalories || Object.keys(existingQueries).length === 0) {
+    goal.leftover.calories = leftover;
+    leftover -= newQueryCalories;
+
+    return true;
+  }
+
+  return false;
+}
+
+function showCards() {
+  mealList.empty();
+
+  let queries = JSON.parse(localStorage.getItem("queries")) || {};
+  for (var [key, value] of Object.entries(queries)) {
+
+    const queryCard = `
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">${key}</h5>
+        <p class="card-text">Calories: ${value.calories}</p>
+        <p class="card-text">Carbs: ${value.carbs}</p>
+        <p class="card-text">Fat: ${value.fat}</p>
+        <p class="card-text">Protein: ${value.protein}</p>
+        <a href="#" class="btn btn-primary">Check Recipe!</a>
+      </div>
+    </div>`
+    mealList.append(queryCard);
+  };
+}
+
+showCards()
