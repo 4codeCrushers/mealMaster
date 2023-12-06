ddf8aa49320bcb35b58b91e7850eeaa79cf9bece
Ewelina Sobora <e@e-dot.uk>
2023-12-06 20:31:25 +0000

    Replace simple js with jquery

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,27 @@
 // Save the selected day to localStorage
-document.addEventListener("DOMContentLoaded", function () {
-    var dayElements = document.querySelectorAll('.day');
-    dayElements.forEach(function (dayElement) {
-        dayElement.addEventListener('click', function () {
-            var dayOfMonth = dayElement.querySelector('.card-title').innerText;
+$(document).ready(function () {
+    var dayElements = $('.day');
+    dayElements.each(function () {
+        $(this).on('click', function () {
+            var dayOfMonth = $(this).find('.card-title').text();
             localStorage.setItem('selectedDay', dayOfMonth); // selected day is stored in dayOfMonth var
         });
     });
-    // Displays todays date
+    // Displays today's date
     var date = dayjs().format("MMMM YYYY");
     $("#today").text(date);
     var totalDays = dayjs().daysInMonth();
-    var calendarContainer = $("#calendar");   
-    // Adds cards based on the number of days in current month
+    var calendarContainer = $("#calendar");
+    // Adds cards based on the number of days in the current month
     for (var day = 1; day <= totalDays; day++) {
         var newCard = $('<div class="card day" style="width: 9rem; height: 9rem;">' +
-                            '<div class="card-body">' +
-                                '<h5 class="card-title">' + day + '</h5>' +
-                                '<p class="card-text"></p>' +
-                                '<img src="assets/images/add.png" class="card-img add-icon mx-auto" style="display: none;" alt="...">' +
-                                '<a href="#" class="stretched-link"></a>' +
-                            '</div>' +
-                        '</div>');
+            '<div class="card-body">' +
+            '<h5 class="card-title">' + day + '</h5>' +
+            '<p class="card-text"></p>' +
+            '<img src="assets/images/add.png" class="card-img add-icon mx-auto" style="display: none;" alt="...">' +
+            `<a href="#" class="stretched-link"></a>` +
+            '</div>' +
+            '</div>');
         // Date is stored locally when card is clicked
         newCard.click(function () {
             var selectedDay = $(this).find('.card-title').text();
@@ -29,4 +29,4 @@ document.addEventListener("DOMContentLoaded", function () {
         });
         calendarContainer.append(newCard);
     }
-});   
+});
