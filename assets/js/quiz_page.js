7a0ff5aac97825e7005e0609b5ee3db0bd68dbba
Dom B <dominicbrown4@hotmail.com>
2023-12-07 18:25:10 +0000

    adding updated JS and HTML files with correct indentation

--- assets/js/quiz_page.js
+++ assets/js/quiz_page.js
@@ -1,14 +1,14 @@
 // hiding all questions
 
 function hideAll () {
-    $('.nutritionQuestion, .mealQuestion, .allergiesQuestion, .nutritionButton, .mealButton, .allergiesButton').hide();
+  $('.nutritionQuestion, .mealQuestion, .allergiesQuestion, .nutritionButton, .mealButton, .allergiesButton').hide();
 }
 
 // function to show Qs when ready
 function showQuestionAndButton (questionClass, buttonClass) {
-    hideAll ();
-    $(questionClass).show();
-    $(buttonClass).show();
+  hideAll ();
+  $(questionClass).show();
+  $(buttonClass).show();
 }
 
 // Code to only show Q1 when quiz page loads
@@ -20,46 +20,46 @@ showQuestionAndButton('.nutritionQuestion, .nutritionButton');
 
 $('.nutritionButton button').on('click', function () {
 
-    var quizGoal = {
-        calories: $('#caloriesInput').val(),
-        protein: $('#proteinInput').val(),
-        carbohydrates: $('#carbohydratesInput').val(),
-        fat: $('#fatInput').val(),
-    }
+  var quizGoal = {
+    calories: $('#caloriesInput').val(),
+    protein: $('#proteinInput').val(),
+    carbohydrates: $('#carbohydratesInput').val(),
+    fat: $('#fatInput').val(),
+  }
 
-    var feedbackNutrition = $('.nutritionQuestion .invalid-feedback');
+  var feedbackNutrition = $('.nutritionQuestion .invalid-feedback');
 
-    if (quizGoal.calories < 1200 || quizGoal.calories > 5000) {
-        feedbackNutrition.eq(0).show();
-    } else {
-        feedbackNutrition.eq(0).hide();
-    }
+  if (quizGoal.calories < 1200 || quizGoal.calories > 5000) {
+    feedbackNutrition.eq(0).show();
+  } else {
+    feedbackNutrition.eq(0).hide();
+  }
 
-    if (quizGoal.protein < 50 || quizGoal.protein > 500) {
-        feedbackNutrition.eq(1).show();
-    } else {
-        feedbackNutrition.eq(1).hide();
-    }
+  if (quizGoal.protein < 50 || quizGoal.protein > 500) {
+    feedbackNutrition.eq(1).show();
+  } else {
+    feedbackNutrition.eq(1).hide();
+  }
 
-    if (quizGoal.carbohydrates < 50 || quizGoal.carbohydrates > 500) {
-        feedbackNutrition.eq(2).show();
-    } else {
-        feedbackNutrition.eq(2).hide();
-    }
+  if (quizGoal.carbohydrates < 50 || quizGoal.carbohydrates > 500) {
+    feedbackNutrition.eq(2).show();
+  } else {
+    feedbackNutrition.eq(2).hide();
+  }
 
-    if (quizGoal.fat < 50 || quizGoal.fat > 500) {
-        feedbackNutrition.eq(3).show();
-    } else {
-        feedbackNutrition.eq(3).hide();
-    }
+  if (quizGoal.fat < 50 || quizGoal.fat > 500) {
+    feedbackNutrition.eq(3).show();
+  } else {
+    feedbackNutrition.eq(3).hide();
+  }
 
-    if (feedbackNutrition.filter(':visible').length  >0) {
-        return;
-    }
+  if (feedbackNutrition.filter(':visible').length  >0) {
+    return;
+  }
 
-    localStorage.setItem('quizGoal', JSON.stringify(quizGoal));
-    showQuestionAndButton('.mealQuestion, .mealButton');
-
+  localStorage.setItem('quizGoal', JSON.stringify(quizGoal));
+  showQuestionAndButton('.mealQuestion, .mealButton');
+    
 });
 
 
@@ -67,16 +67,16 @@ $('.nutritionButton button').on('click', function () {
 
 $('.mealButton button').on('click', function () {
 
-    var numberOfMeals = parseInt($('#mealsInput').val(), 10);
-    var feedbackMeals = $('.mealQuestion .invalid-feedback');
+  var numberOfMeals = parseInt($('#mealsInput').val(), 10);
+  var feedbackMeals = $('.mealQuestion .invalid-feedback');
 
-    if (numberOfMeals < 1 || numberOfMeals > 10) {
-        feedbackMeals.show();
-        return;
+  if (numberOfMeals < 1 || numberOfMeals > 10) {
+    feedbackMeals.show();
+    return;
     } else {
-        feedbackMeals.hide();
-        localStorage.setItem('numberOfMeals', numberOfMeals);
-        showQuestionAndButton('.allergiesQuestion, .allergiesButton');
+      feedbackMeals.hide();
+      localStorage.setItem('numberOfMeals', numberOfMeals);
+      showQuestionAndButton('.allergiesQuestion, .allergiesButton');
     }
 });
 
@@ -84,20 +84,20 @@ $('.mealButton button').on('click', function () {
 
 $('.allergiesButton button').on('click', function () {
 
-    var foodAllergies = $('#allergiesInput').val();
-    var feedbackAllergies = $('.allergiesQuestion .invalid-feedback');
+  var foodAllergies = $('#allergiesInput').val();
+  var feedbackAllergies = $('.allergiesQuestion .invalid-feedback');
 
-    // Only allow text in input box 
+  // Only allow text in input box 
 
-    var lettersOnlyRegex = /^[a-zA-Z\s]+$/;
+  var lettersOnlyRegex = /^[a-zA-Z\s]+$/;
 
-    if (!lettersOnlyRegex.test(foodAllergies)) {
-        feedbackAllergies.show();
-        return;
+  if (!lettersOnlyRegex.test(foodAllergies)) {
+    feedbackAllergies.show();
+    return;
     } else {
-        feedbackAllergies.hide();
-        localStorage.setItem('foodAllergies', foodAllergies);
-        window.location.href = 'search.html';
+      feedbackAllergies.hide();
+      localStorage.setItem('foodAllergies', foodAllergies);
+      window.location.href = 'search.html';
     }
     
 });
