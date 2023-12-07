7ae2fd90f82694ef69f0936c2c17de4173448f38
Ewelina Sobora <e@e-dot.uk>
2023-12-07 21:10:04 +0000

    Fix modals

--- assets/js/mealList.js
+++ assets/js/mealList.js
@@ -8,7 +8,7 @@ const nutrition = nutritionInfo();
 // Get the <span> element that closes the modal
 const span = $(".close")[0];
 // Get the modal
-const modal = $("#alertMsg");
+const modal = $("#alert-modal");
 const modalMessage = $(".modal-message");
 const modalContent = $(".modal-content");
 // Load page content
@@ -205,4 +205,3 @@ window.onclick = function (event) {
     modal.css("display", "none");
   }
 }
-
