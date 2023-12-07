1e4cca2039fac61b78c54ed988f882bf2060c2ce
Tom Paling <137108048+ph8tfunk@users.noreply.github.com>
2023-12-07 22:16:32 +0000

    refactored code in functions. If searched meal is not found return random meal

--- assets/js/mealView.js
+++ assets/js/mealView.js
@@ -14,54 +14,59 @@ function getRandomMeal () {
             return response.json();
         })
         .then(function(data){
-            var imgEl = $("<img>");
-            imgEl.attr('class', 'randomMealImage');
-            var mealName = $('<h5> ' + data.meals[0].strMeal + ' </h5>');
-            var pEl1 = $('<p> ' + data.meals[0].strCategory + ", " + data.meals[0].strArea + '</p>');
-            imgEl.attr('src', data.meals[0].strMealThumb);
-            rnMeal.append(mealName, pEl1, imgEl);
-            var ingredientsTitle = $('<h5 class="ingredients-title">Ingredients:</h5>');
-            rnMeal.append(ingredientsTitle);
-            ingredientsTitle.css('margin-top', '10px');
-            
-            //API returns upto 20 Ingredients and measurements
-
-
-      //API returns upto 20 Ingredients and measurements
-
-      for (let index = 1; index <= 20; index++) {
-        recipe.push({ "Ingredient": data.meals[0]['strIngredient' + index], "Measure": data.meals[0]['strMeasure' + index] });
-      }
-
-
-
-            // if not null display the following
-            if (data.meals[0].strYoutube) {
-                var buttonEl = $('<a href="' + data.meals[0].strYoutube + '" class="btn btn-primary btn-danger" target="_blank">');
-                var iconEl = $('<img src="assets/images/youtube.svg" alt="Icon" class="icon">');
-                buttonEl.append(iconEl);
-                buttonEl.append(" Youtube");
-                buttonEl.css('margin-right', '10px')
-                $('#randomMeal').append(buttonEl);
-              }
-
-            if (data.meals[0].strSource){
-                var aElSrc = $('<a href="'+data.meals[0].strSource + '" class="btn btn-primary" target="_blank">');
-                aElSrc.text("Recipe");
-                rnMeal.append(aElSrc);
-            }
- 
-
-      for (let index = 0; index < recipe.length; index++) {
-        if (recipe[index].Ingredient === "") {
-          break;
-        }
-        var pEl = $('<p>' + recipe[index].Ingredient + ' : ' + recipe[index].Measure + '</p>');
-        rnMeal.append(pEl);
-      }
-
+        displayMeal(data);
     })
   }
+
+function displayMeal (data){
+
+  
+  var imgEl = $("<img>");
+  imgEl.attr('class', 'randomMealImage');
+  var mealName = $('<h5> ' + data.meals[0].strMeal + ' </h5>');
+  var pEl1 = $('<p> ' + data.meals[0].strCategory + ", " + data.meals[0].strArea + '</p>');
+  imgEl.attr('src', data.meals[0].strMealThumb);
+  rnMeal.append(mealName, pEl1, imgEl);
+  var ingredientsTitle = $('<h5 class="ingredients-title">Ingredients:</h5>');
+  rnMeal.append(ingredientsTitle);
+  ingredientsTitle.css('margin-top', '10px');
+  
+  //API returns upto 20 Ingredients and measurements
+
+
+//API returns upto 20 Ingredients and measurements
+
+  for (let index = 1; index <= 20; index++) {
+    recipe.push({ "Ingredient": data.meals[0]['strIngredient' + index], "Measure": data.meals[0]['strMeasure' + index] });
+  }
+
+
+
+  // if not null display the following
+  if (data.meals[0].strYoutube) {
+      var buttonEl = $('<a href="' + data.meals[0].strYoutube + '" class="btn btn-primary btn-danger" target="_blank">');
+      var iconEl = $('<img src="assets/images/youtube.svg" alt="Icon" class="icon">');
+      buttonEl.append(iconEl);
+      buttonEl.append(" Youtube");
+      buttonEl.css('margin-right', '10px')
+      $('#randomMeal').append(buttonEl);
+  }
+
+  if (data.meals[0].strSource){
+      var aElSrc = $('<a href="'+data.meals[0].strSource + '" class="btn btn-primary" target="_blank">');
+      aElSrc.text("Recipe");
+      rnMeal.append(aElSrc);
+  }
+
+
+  for (let index = 0; index < recipe.length; index++) {
+    if (recipe[index].Ingredient === "") {
+    break;
+    }
+  var pEl = $('<p>' + recipe[index].Ingredient + ' : ' + recipe[index].Measure + '</p>');
+  rnMeal.append(pEl);
+  }
+}
 
 
 // - API call to get image based on ingredient
@@ -69,7 +74,7 @@ function getSelectedMeal () {
   return localStorage.getItem("selectedMeal");
 }
 
-function getImageFromIngredient () {
+function getMeal () {
 
   var getMeal = getSelectedMeal ();  
   var randomMeal_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s="+getMeal;
@@ -79,10 +84,13 @@ function getImageFromIngredient () {
       return response.json();
     })
     .then(function (data) {
-      if (!data.meals)
-      console.log("no food!!");
+      if (!data.meals){
+        console.log("not found return random receipe");
+        getRandomMeal();
+      } else {
+        displayMeal(data);
+      }
     })
 }
 
-getRandomMeal();
-getImageFromIngredient ()
+getMeal();
